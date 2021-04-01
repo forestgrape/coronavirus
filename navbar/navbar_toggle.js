8b7301ba684cd9fb756c9240b32c3bf56baef050
witotraub@runbox.com <witotraub@runbox.com>
2021-04-01 14:05:46 +0200

    moved from classes to ids for nav-elem and content

--- navbar/navbar_toggle.js
+++ navbar/navbar_toggle.js
@@ -1,24 +1,20 @@
 function hideNav(){
     //hide side-nav
-    const navs = document.getElementsByClassName('side-nav')
-    navs[0].style.display = "none"
-    //expand main-content
-    const mains = document.getElementsByClassName('main-content')
-    mains[0].setAttribute("style","margin-left:0%")
+    document.getElementById('side-nav').style.display = "none"
+    //expand content
+    const content = document.getElementById('central-content')
+    content.setAttribute("style","margin-left:0%")
     //display menu
-    const menubutton = document.getElementById('menu-button')
-    menubutton.style.display = "block"
+    document.getElementById('menu-button').style.display = "block"
 }
 
 function showNav(){
     //console.log('show')
-    const navs = document.getElementsByClassName('side-nav')
-    navs[0].style.display = "block"
-    //shrink main-content
-    const mains = document.getElementsByClassName('main-content')
+    document.getElementById('side-nav').style.display = "block"
+    //shrink content 
+    const content = document.getElementById('central-content')
     //in this order : 9% + 9rem!!
-    mains[0].setAttribute("style",'margin-left:calc(9% + 9rem)')
+    content.setAttribute("style",'margin-left:calc(9% + 9rem)')
     //hide menu
-    const menubutton = document.getElementById('menu-button')
-    menubutton.style.display = "none"
+    document.getElementById('menu-button').style.display = "none"
 }
