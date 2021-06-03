138aecebb19f6560e992385cbfe741049ef83940
witotraub <witotraub@runbox.com>
2021-06-03 14:52:05 +0200

    clarified nav-scroll-hide and should work on touch now

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -23,16 +23,19 @@ function showNav() {
     document.getElementById('menu-button').style.display = "none"
 }
 
+const side_nav = document.getElementById('side-nav')
 
-function mouseOverSideNav() {
+function showSideNavScrollbar() {
     /*show the the nav scroll bar if necessary*/
     const TITLE_OFFSET = 170;
-    const side_nav = document.getElementById('side-nav')
     if (side_nav.offsetHeight < document.getElementById('nav-toc').offsetHeight + TITLE_OFFSET) {
         //console.log('show overflow')
         side_nav.setAttribute("style", "overflow-y:auto")
     }
+}
 
+function hideSideNavScrollbar() {
+    side_nav.setAttribute("style", "overflow-y:hidden")
 }
 
 function onContentMove() {
@@ -40,18 +43,13 @@ function onContentMove() {
     * scrolls the nav element if necessary
     * hide the nav scroll bar //would be only necessary on desktop on mobile usually auto hidden
     */
-    const side_nav = document.getElementById('side-nav')
-    console.log('content move')
     
     if (side_nav.style.display != "none") {
         const lowest_placement = parseInt(.8 * side_nav.offsetHeight)
-        
-
-        side_nav.setAttribute("style", "overflow-y:hidden")
         const active_elem = document.getElementsByClassName('is-active-link')[0]
         const loc_of_active = active_elem.offsetTop      //alternative would be: getBoundingClientRect().top //gets position respect to view port
         //console.log('top:'+active_elem + parseInt(loc_of_active))
-        /*usually works; smooths seems disabled in safari*/
+        /*usually works; ?option smooth in safari?*/
         side_nav.scrollTo({ top: Math.max(0, loc_of_active - lowest_placement), 'behavior': 'smooth' })
     }
 }
